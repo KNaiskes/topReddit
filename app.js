36c926406601c1e92caf90bda46d9ce7b5f60cf4
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-11-02 21:40:20 +0200

    added html template for the posts titles

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 var request = require('request');
 var app = express()
 
+app.set("view engine", "ejs");
+
 const POSTS_NUMBER = 2;
 const REDDIT = "https://www.reddit.com/r/";
 const TOPICS = ["linux", "programming"]
@@ -9,10 +11,9 @@ const LIMIT = ".json?limit=" + POSTS_NUMBER;
 
 const PORT = 8080;
 
-app.get("/", (req, res) => res.send("Testing"))
-app.listen(PORT);
 
 var link = "";
+var results = [];
 
 for(l in TOPICS) {
 	link = REDDIT + TOPICS[l] + LIMIT;
@@ -20,7 +21,16 @@ for(l in TOPICS) {
 	request(link, function (err, res, json) {
 		json = JSON.parse(json);
 		for(i = 0; i < POSTS_NUMBER; i++) {
-			console.log(json.data.children[i].data.title); 
+			//console.log(json.data.children[i].data.title); 
+			results.push(json.data.children[i].data.title);
 		}
 	});
 }
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+	res.render("index", {results: results} );
+});
+
+app.listen(PORT);
